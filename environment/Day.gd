3fa91657968cc6d73d3e510e6713fc984e993d3e
gramozilho <miguelsemartins+1@gmail.com>
2020-03-14 18:58:05 +0000

    fog added

---
+++ environment/Day.gd
@@ -0,0 +1,20 @@
+extends WorldEnvironment
+
+var fog_level = 0
+
+func adjust_fog(lvl):
+	if lvl == 2:
+		environment.fog_enabled = false
+	elif lvl == 1:
+		environment.fog_enabled = true
+		environment.fog_depth_begin = 10
+		environment.fog_depth_end = 50
+	else:
+		environment.fog_enabled = true
+		environment.fog_depth_begin = 5
+		environment.fog_depth_end = 30
+
+func cycle_fog():
+	fog_level = (fog_level+1)%-3
+	adjust_fog(fog_level)
+	print('fog ', fog_level)
