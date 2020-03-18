db6453aa9038f7008716930893a5ca31ae33c05e
gramozilho <miguelsemartins+1@gmail.com>
2020-03-18 09:01:51 +0000

    fixes to music player

--- Jukebox.gd
+++ Jukebox.gd
@@ -11,6 +11,7 @@ var queue_track = []
 # Needs day/night
 # Needs one-off events (new land, near land, etc)
 var switch_time = 9.29
+var day_time = true
 
 func _ready():
 	randomize()
@@ -28,7 +29,7 @@ func next_song():
 		queue_track.remove(0)
 	else:
 		if near_island > 0:
-			current = $FindingLand
+			current = $NearLand
 		else:
 			var up_next = []
 			if (randf() < change_of_random_fill): # and (near_island>0)
@@ -45,26 +46,33 @@ func next_song():
 	current.play()
 	
 	# Update timer
-	print(current.stream.get_length())
+	#print(current.stream.get_length())
 	$SwitchTimer.wait_time = current.stream.get_length() - (.71 + .838708)
 	$SwitchTimer.start()
 
 func update_near_island(amount):
-	#if (near_island == 0) and (amount == 1) and (current != $NearLand) and !($NearLand in queue_track):
-	#	queue_track.append($FindingLand)
+	if (near_island == 0) and (amount == 1) and (current != $FindingLand) and !($FindingLand in queue_track):
+		queue_track.append($FindingLand)
+	elif (near_island > 0) and (amount == -1):
+		queue_track.append($LeavingLand)
 	near_island = max(0, near_island + amount)
 	print('Near island: ', near_island)
 
 func dock_on_port():
 	print('Docking!')
-	if !($FindingLand in queue_track):
-		queue_track.append($NearLand)
+	#if !($FindingLand in queue_track):
+	#	queue_track.append($NearLand)
 	
 func leave_port():
-	print('Leaving!')
-	if !($LeavingLand in queue_track):
-		queue_track.append($LeavingLand)
-
+	print('Leaving dock!')
+	#if !($LeavingLand in queue_track):
+	#	queue_track.append($LeavingLand)
 
 func _on_SwitchTimer_timeout():
 	next_song()
+
+func switch_to_night():
+	queue_track.append($Night/Intro)
+
+func switch_to_day():
+	queue_track.append($Day/Intro)
