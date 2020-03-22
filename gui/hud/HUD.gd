543554de64588b317ad026a93862df8edd95f4da
dalton5000 <marius.stieme@gmail.com>
2020-03-22 17:17:12 +0100

    add relic counter to hud

--- gui/hud/HUD.gd
+++ gui/hud/HUD.gd
@@ -3,14 +3,13 @@ extends Control
 var health = 100 setget set_health
 var money = 0 setget set_money
 var speed = 0 setget set_speed
-var ribbons_collected = 0 setget set_ribbons_collected
+
+var relics_collected = 0 setget set_relics_collected
 
 onready var health_label = $Mainpanel/HealthLabel
 onready var money_label = $Mainpanel/MoneyLabel
 
-onready var ribbons_panel = $RibbonsPanel
-onready var ribbons = $RibbonsPanel/VBoxContainer.get_children()
-onready var ribbons_tween = $RibbonsPanel/Tween
+onready var relics_label = $RibbonsPanel/Relic/RelicCounterLabel
 
 func set_health(h):
 	health = int(h)
@@ -23,8 +22,6 @@ func set_money(m):
 func set_speed(s):
 	speed = int(s)
 
-func set_ribbons_collected(r):
-	if r > ribbons_collected:
-		ribbons_collected = r
-		#move ribbon panel with tween
-		#load ribbon icon
+func set_relics_collected(r):
+	relics_collected = int(r)
+	relics_label.text = str(relics_collected)
