2b8ee1bed274cc642d4db4222785f62ad8130838
awindahl <awindahl@kth.se>
2020-03-22 19:08:02 +0100

    merge

--- gui/TitleMenu.gd
+++ gui/TitleMenu.gd
@@ -5,13 +5,13 @@ onready var settings_panel = $SettingsPanel
 onready var seed_edit = $HBoxContainer/SeedEdit
 
 func _on_StartButton_button_up():
-	pass # Replace with function body.
+	get_tree().change_scene("res://Main.tscn")
 
 func _on_SettingsButton_button_up():
 	$SettingsPanel.show()
 
 func _on_QuitButton_button_up():
-	pass # Replace with function body.
+	get_tree().quit(-1)
 
 
 func _on_CloseSettingsButton_button_up():
